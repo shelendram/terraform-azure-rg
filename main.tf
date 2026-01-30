85b4ca7dbde5e22ee75180f81c9704178587505f
shelendram <shailccnp@gmail.com>
2026-01-31 00:16:50 +0530

    vm creation  main.tf

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstate123ab"   # ← Azure Portal wala exact name
+    storage_account_name = "tfstate123ab"
     container_name       = "tfstate"
-    key                  = "terraform-azure-rg.tfstate"
+    key                  = "linux-vm.tfstate"
   }
 }
 
@@ -18,7 +18,83 @@ provider "azurerm" {
   features {}
 }
 
+# -------------------------
+# Resource Group
+# -------------------------
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+# -------------------------
+# Existing Key Vault
+# -------------------------
+data "azurerm_key_vault" "kv" {
+  name                = "linux-vm-key-vault"
+  resource_group_name = "rg-keyvault"
+}
+
+data "azurerm_key_vault_secret" "vm_password" {
+  name         = "linux-admin-password"
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
+
+# -------------------------
+# Networking
+# -------------------------
+resource "azurerm_virtual_network" "vnet" {
+  name                = "linux-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.20.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "linux-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.20.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "linux-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# -------------------------
+# Linux VM (password from Key Vault)
+# -------------------------
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "linux-vm-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = "Standard_B1s"
+
+  admin_username = "azureuser"
+  admin_password = data.azurerm_key_vault_secret.vm_password.value
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+

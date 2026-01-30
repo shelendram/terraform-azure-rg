bd3aa1bdade2c7f069b69c8dfa3213dd73e4d3a2
shelendram <shailccnp@gmail.com>
2026-01-31 01:50:12 +0530

    vm size updated

--- main.tf
+++ main.tf
@@ -91,10 +91,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-noble"
-    sku       = "24_04-lts-gen2"
-    version   = "latest"
-  }
+  publisher = "Canonical"
+  offer     = "0001-com-ubuntu-server-jammy"
+  sku       = "22_04-lts-gen2"
+  version   = "latest"
+}
   
 }

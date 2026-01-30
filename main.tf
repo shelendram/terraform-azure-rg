83fb6adbc2f156fa14730e49e0b9400dff5acc0b
shelendram <shailccnp@gmail.com>
2026-01-30 12:41:47 +0530

    storage account added

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstate123ab"   # ← Azure Portal wala exact name
+    container_name       = "tfstate"
+    key                  = "terraform-azure-rg.tfstate"
+  }
 }
 
 provider "azurerm" {
